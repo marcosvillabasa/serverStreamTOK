f64f19006daa7109b9e1201610b28226c0c2760f
MarcosVillabasa <marcos.villabasa@autocity.com.ar>
2019-12-28 15:08:17 -0300

    agregado script

---
+++ config/config.js
@@ -0,0 +1,23 @@
+//port
+process.env.PORT = process.env.PORT || 3000;
+
+//jwt expiration
+process.env.EXPIRATION_TOKEN = 60 * 60 * 24 * 30;
+
+//seed
+process.env.SEED = process.env.SEED || "seed-secret";
+
+//client id
+process.env.CLIENT_ID =
+  process.env.CLIENT_ID ||
+  "653031729607-3d0esm0q7qcpnb249vdaiv4i6jtrkacr.apps.googleusercontent.com";
+
+// endpoint = '/api';
+
+const config = {
+  endpoint: "/api",
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 3000
+};
+
+module.exports = config;
