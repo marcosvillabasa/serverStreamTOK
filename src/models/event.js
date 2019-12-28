871d6b80c14ccae9fc181d63e9970ee42bcc3dc5
MarcosVillabasa <marcos.villabasa@autocity.com.ar>
2019-12-28 14:26:29 -0300

    creado modelo para evento

---
+++ src/models/event.js
@@ -0,0 +1,36 @@
+const joi = require('@hapi/joi');
+const mongoose = require('mongoose');
+let uniqueValidator = require('mongoose-unique-validator');
+
+let Schema = mongoose.Schema;
+
+let eventSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, "event name is required"],
+        minlength: 5,
+        maxlength: 50
+    },
+    dateStart: {
+        type: Date,
+        required: true
+    },
+    dateEnd: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: Boolean,
+        default: true
+    }
+
+});
+
+module.exports = mongoose.model('Event', eventSchema);
