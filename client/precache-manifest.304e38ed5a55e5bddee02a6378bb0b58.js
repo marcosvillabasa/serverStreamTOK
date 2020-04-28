6d62f3b7ef90f59df9575d5665fadaf02ff2ea9c
MarcosVillabasa <marcos.villabasa@autocity.com.ar>
2020-04-28 20:59:04 -0300

    cambios server

---
+++ client/precache-manifest.304e38ed5a55e5bddee02a6378bb0b58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44fafcac66dfaae733c19babb756d6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7674b567ffbc1497b4f5",
+    "url": "/static/css/main.ddb221bd.chunk.css"
+  },
+  {
+    "revision": "b624ac427bb57f4570fc",
+    "url": "/static/js/2.1d7b6641.chunk.js"
+  },
+  {
+    "revision": "73138c634db8d37252cbabcb33689813",
+    "url": "/static/js/2.1d7b6641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7674b567ffbc1497b4f5",
+    "url": "/static/js/main.75660372.chunk.js"
+  },
+  {
+    "revision": "abc2f647228bd6983716",
+    "url": "/static/js/runtime-main.14c6129f.js"
+  }
+]);
