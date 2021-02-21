0be8e6821c3576c128330cc031e740e36ba8a913
Marcos Villabasa <marcosvillabasa@gmail.com>
2021-02-21 15:01:19 -0300

    modificaciones front

--- client/service-worker.js
+++ client/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d5a7649db3b28f8aed50f51062bf09e.js"
+  "/precache-manifest.c5a4b598700e8ac3c6621d7d40a98492.js"
 );
 
 self.addEventListener('message', (event) => {
